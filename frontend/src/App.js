056552674ca8ff1a2603aa02130db59510e356dc
Shrey Marwaha <shrey19334@iiitd.ac.in>
2022-10-20 04:18:36 +0530

    chaned bootstrap css position

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,4 @@
 import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
 import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom'
 import Home from './pages/Home'
 
