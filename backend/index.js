fb97a21d33d3e16ffe6d973f54d44b8e41336581
Shrey Marwaha <shrey19334@iiitd.ac.in>
2022-10-29 22:00:59 +0530

    added login page

---
+++ backend/index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const cors = require('cors')
+const BodyParser = require('body-parser')
+require('dotenv').config()
+const app = express()
+app.use(cors())
+app.use(BodyParser.json())
+
+var mysql = require('mysql')
+var con = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: 'Password@123',
+	database: 'fcs',
+})
+
+con.connect(function (err) {
+	if (err) throw err
+})
+
+const port = process.env.PORT || 5000
+app.listen(port, (err) => (err ? console.log('Filed to Listen on Port', port) : console.log('Listing for Port', port)))
+
+app.get('/api/users', (req, res) => {
+	con.query('SELECT * FROM users', (err, data) => {
+		if (err) throw err
+		res.json(data)
+	})
+})
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,43 @@
 import './App.css'
-import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom'
-import Home from './pages/Home'
+import {BrowserRouter as Router, Routes, Route, Navigate, Link} from 'react-router-dom'
+import Login from './pages/Login'
+import SignUp from './pages/SignUp'
 
 function App() {
-	function handleClick(e) {
-		e.preventDefault()
-		console.log('The button was clicked.')
-	}
 	return (
 		<Router>
-			<Routes>
-				<Route exact path="/" element={<Home />}></Route>
-				<Route path="*" element={<Navigate replace to="/" />} />
-			</Routes>
+			<div className="App">
+				<nav className="navbar navbar-expand-lg navbar-light fixed-top">
+					<div className="container">
+						<Link className="navbar-brand" to={'/sign-in'}>
+							61x Patient Management
+						</Link>
+						<div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+							<ul className="navbar-nav ml-auto">
+								<li className="nav-item">
+									<Link className="nav-link" to={'/sign-in'}>
+										Login
+									</Link>
+								</li>
+								<li className="nav-item">
+									<Link className="nav-link" to={'/sign-up'}>
+										Sign up
+									</Link>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</nav>
+				<div className="auth-wrapper">
+					<div className="auth-inner">
+						<Routes>
+							<Route exact path="/" element={<Login />} />
+							<Route path="/sign-in" element={<Login />} />
+							<Route path="/sign-up" element={<SignUp />} />
+						</Routes>
+					</div>
+				</div>
+			</div>
 		</Router>
 	)
 }
