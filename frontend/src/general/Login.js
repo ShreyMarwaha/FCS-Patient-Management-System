c7be903d214a4861042fd574ed1660d5d9cc948d
= <=>
2022-10-30 01:01:34 +0530

    API test 1

--- frontend/src/general/Login.js
+++ frontend/src/general/Login.js
@@ -1,6 +1,13 @@
 import React from 'react'
 
 function Login() {
+
+	fetch('https://192.168.2.235/api/users').then((res) => {
+		res.json().then((data) => {
+            console.log(data)
+		})
+    })
+	
 	return (
 		<form>
 			<h3>Sign In</h3>
