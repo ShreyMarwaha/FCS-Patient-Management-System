7a92ede7b00b9ff33ea9aa6322423931e41f35fd
Shrey Marwaha <shrey19334@iiitd.ac.in>
2022-11-01 21:31:15 +0530

    login-signup authentication

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@
 
 const express = require('express')
 const cors = require('cors')
+const bcrypt = require('bcrypt')
 const BodyParser = require('body-parser')
 require('dotenv').config()
 const app = express()
@@ -22,7 +23,6 @@ con.connect(function (err) {
 
 const port = process.env.PORT || 5000
 app.listen(port, (err) => (err ? console.log('Failed to Listen on Port ', port) : console.log('Listening for Port ', port)))
-
 
 // API Definitions /////////////////////////////////////////////////
 
@@ -96,5 +96,31 @@ app.get('/api/deletepha', (req, res) => {
 	con.query(`DELETE FROM pharmacy WHERE id=${val}`, (err, data) => {
 		if (err) throw err
 		res.json({data})
+	})
+})
+
+app.post('/api/signup', (req, res) => {
+	const salt = bcrypt.genSaltSync(10)
+	const password = bcrypt.hashSync(req.body.password, salt)
+	con.query(`INSERT INTO users (id, name, email, role, password, salt) VALUES ('${req.body.uuid}','${req.body.name}', '${req.body.email}', '${req.body.registration_type}', '${password}', '${salt}')`, (err) => {
+		if (err) throw err
+		res.status(400)
+	})
+})
+
+app.post('/api/authenticate', (req, res) => {
+	const email = req.body.email
+	const entered_password = req.body.password
+	con.query(`SELECT password, salt FROM users WHERE email="${email}"`, (err, data) => {
+		if (err) throw err
+		if (data.length === 0) res.json({status: 'user not found'})
+
+		const salt = data[0].salt
+		const password = bcrypt.hashSync(entered_password, salt)
+		if (password === data[0].password) {
+			res.json({status: 'success'})
+		} else {
+			res.json({status: 'failed'})
+		}
 	})
 })
