dc950fe1167afa2a53337df0731e7917a9ac970b
Shrey Marwaha <shrey19334@iiitd.ac.in>
2022-11-23 02:06:05 +0530

    wait for admin approval

--- frontend/src/general/Login.js
+++ frontend/src/general/Login.js
@@ -6,7 +6,7 @@ import {DataContext} from '../App'
 
 function Login() {
 	const {loggedIn, setLoggedIn} = useContext(DataContext)
-	const [error, setError] = React.useState(false)
+	const [error, setError] = React.useState({isError: false, message: ''})
 	function handleSubmit(event) {
 		event.preventDefault()
 
@@ -23,10 +23,18 @@ function Login() {
 		})
 			.then((response) => response.json())
 			.then((data) => {
-				if (data.status === 'success') {
-					setLoggedIn(1)
-					setError(false)
-				} else setError(true)
+				if (data.authentication === 'success') {
+					if (data.status === 1) {
+						setLoggedIn(1)
+						setError({isError: false, message: ''})
+					} else if (data.status === 0) {
+						console.log('User not verified')
+						setError({isError: true, message: 'User not verified. Please wait for the admin to verify your account.'})
+					} else if (data.status === 2) {
+						console.log('User blocked')
+						setError({isError: true, message: 'User blocked. Please contact the admin.'})
+					}
+				} else setError({isError: true, message: 'Incorrect email or password'})
 			})
 			.catch((error) => {
 				console.error('Error:', error)
@@ -39,18 +47,30 @@ function Login() {
 				<div>
 					<h3 style={{color: 'var(--dark-green)'}}>Login</h3>
 					<Form.Group className="mb-3" controlId="form_email" style={{paddingTop: 100}}>
-						<Form.Control type="email" placeholder="Enter email" />
+						<Form.Control
+							type="email"
+							placeholder="Enter email"
+							onClick={() => {
+								setError({isError: false, message: ''})
+							}}
+						/>
 					</Form.Group>
 
 					<Form.Group className="mb-3" controlId="form_password">
-						<Form.Control type="password" placeholder="Password" />
+						<Form.Control
+							type="password"
+							placeholder="Password"
+							onClick={() => {
+								setError({isError: false, message: ''})
+							}}
+						/>
 					</Form.Group>
 				</div>
 				<div>
 					<Button variant="primary" type="submit" style={{marginBottom: 20, width: 100, backgroundColor: 'var(--dark-green)', border: 0}}>
 						Submit
 					</Button>
-					{error ? <p className="text-danger">Wrong Email or Password</p> : <></>}
+					{error.isError ? <p className="text-danger">{error.message}</p> : <></>}
 					<br></br>
 					<a href="#">Forgot password?</a>
 				</div>
