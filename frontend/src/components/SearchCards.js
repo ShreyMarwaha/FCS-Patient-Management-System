1a46936754d452f0d07f7f5f7a4d2008fde73c67
Shrey Marwaha <shrey19334@iiitd.ac.in>
2022-11-25 02:20:38 +0530

    fixed search

--- frontend/src/components/SearchCards.js
+++ frontend/src/components/SearchCards.js
@@ -2,21 +2,17 @@ import {useEffect} from 'react'
 import {Link} from 'react-router-dom'
 import './components.css'
 
-function SearchCards(d, type) {
-	let l = []
-	useEffect(() => {
-		console.log('useEffect')
-		d.forEach((element) => {
-			let s = '/details?type=' + type + '&id=' + element.id
-			l.push(
-				<Link className="cardlink" to={s}>
-					<div className="card">{element.name}</div>
-				</Link>
-			)
-		})
-	}, [])
-
-	return {l}
+function SearchCards(dataArray, type) {
+	let cardsList = []
+	dataArray.forEach((element) => {
+		let link = '/details?type=' + type + '&id=' + element.id
+		cardsList.push(
+			<Link className="cardlink" to={link} id={element.id}>
+				<div className="card">{element.name}</div>
+			</Link>
+		)
+	})
+	return cardsList
 }
 
 export default SearchCards
--- frontend/src/pages/Search.js
+++ frontend/src/pages/Search.js
@@ -3,46 +3,33 @@ import SearchCards from '../components/SearchCards'
 import './pages.css'
 import {DataContext} from '../App'
 
-let d1 = []
-let d2 = []
-let d3 = []
-
 function Search() {
 	const {loginData, setLoginData} = useContext(DataContext)
-	const [queryRun, setQueryRun] = useState(0)
-	const [dataFound1, setDataFound1] = useState(0)
-	const [dataFound2, setDataFound2] = useState(0)
-	const [dataFound3, setDataFound3] = useState(0)
+	const [dataHospital, setDataHospital] = useState([])
+	const [dataDoctor, setDataDoctor] = useState([])
+	const [dataPharmacy, setDataPharmacy] = useState([])
 
 	function fetchSearchData() {
 		console.log('clicked')
-		setQueryRun(1)
 		var value = document.getElementById('searchValue').value
 		fetch(`https://192.168.2.235/api/searchdocs?name=${value}&jwt=${loginData.data.token}`).then((res) => {
 			res.json().then((data) => {
-				if (data.hasOwnProperty('err') || data.data.length === 0) {
-					setDataFound1(0)
-				} else {
-					d1 = data.data
-					setDataFound1(1)
+				if (!data.hasOwnProperty('err') && data.data.length > 0) {
+					setDataDoctor(data.data)
 				}
 			})
 		})
 		fetch(`https://192.168.2.235/api/searchhospitals?name=${value}&jwt=${loginData.data.token}`).then((res) => {
 			res.json().then((data) => {
-				if (data.data.length === 0) {
-				} else {
-					d2 = data.data
-					setDataFound2(1)
+				if (!data.hasOwnProperty('err') && data.data.length > 0) {
+					setDataHospital(data.data)
 				}
 			})
 		})
 		fetch(`https://192.168.2.235/api/searchpharmacy?name=${value}&jwt=${loginData.data.token}`).then((res) => {
 			res.json().then((data) => {
-				if (data.data.length === 0) {
-				} else {
-					d3 = data.data
-					setDataFound3(1)
+				if (!data.hasOwnProperty('err') && data.data.length > 0) {
+					setDataPharmacy(data.data)
 				}
 			})
 		})
@@ -60,24 +47,18 @@ function Search() {
 			</div>
 
 			<div className="search-results">
-				{queryRun ? (
-					<>
-						<div>
-							Doctors
-							<div className="records">{dataFound1 ? <>{SearchCards(d1, 'doc')}</> : <>No Records Found!</>}</div>
-						</div>
-						<div>
-							Hospitals
-							<div className="records">{dataFound2 ? <>{SearchCards(d2, 'hos')}</> : <>No Records Found!</>}</div>
-						</div>
-						<div>
-							Pharmacies
-							<div className="records">{dataFound3 ? <>{SearchCards(d3, 'pha')}</> : <>No Records Found!</>}</div>
-						</div>
-					</>
-				) : (
-					<></>
-				)}
+				<div>
+					Doctors
+					<div className="records">{dataDoctor.length > 0 ? SearchCards(dataDoctor, 'doc') : <>No Records Found!</>}</div>
+				</div>
+				<div>
+					Hospitals
+					<div className="records">{dataHospital.length > 0 ? SearchCards(dataHospital, 'hos') : <>No Records Found!</>}</div>
+				</div>
+				<div>
+					Pharmacies
+					<div className="records">{dataPharmacy.length > 0 ? SearchCards(dataPharmacy, 'pha') : <>No Records Found!</>}</div>
+				</div>
 			</div>
 		</>
 	)
