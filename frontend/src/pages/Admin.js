b2b5cebfbde4c73b5a8213e95baa0e942c8f0a6d
Shrey Marwaha <shrey19334@iiitd.ac.in>
2022-11-28 17:06:16 +0530

    improved user management

--- frontend/src/pages/Admin.js
+++ frontend/src/pages/Admin.js
@@ -9,53 +9,36 @@ function Admin() {
 	const {loginData, setLoginData} = useContext(DataContext)
 	const [unverifiedUsers, setUnverifiedUsers] = useState([])
 	const [blockedUsers, setBlockedUsers] = useState([])
+	const [verifiedUsers, setVerifiedUsers] = useState([])
 	useEffect(() => {
-		getUnverifiedUsers()
-		getBlockedUsers()
+		getUsers()
 	}, [])
 
-	function getUnverifiedUsers() {
-		fetch(`https://192.168.2.235/api/unverifiedusers?jwt=${loginData.data.token}`).then((res) => {
+	function getUsers() {
+		fetch(`https://192.168.2.235/api/users?jwt=${loginData.data.token}`).then((res) => {
 			res.json().then((data) => {
 				if (data.hasOwnProperty('err') || data.data.length === 0) {
-					console.log('No unverified users present !!')
+					console.log('No users present !!')
 				} else {
-					console.log(data.data)
-					// setUnverifiedUsers(1)
-					// let l = []
-					// data.data.forEach((element) => {
-					// 	let link = '/details?type=user&id=' + element.id
-					// 	l.push(
-					// 		<Link className="cardlink" to={link} id={element.id}>
-					// 			<div className="card">{element.name}</div>
-					// 		</Link>
-					// 	)
-					// })
-					setUnverifiedUsers(data.data)
-					// console.log(l)
+					setUnverifiedUsers(data.data.filter((user) => user.status === 0))
+					setVerifiedUsers(data.data.filter((user) => user.status === 1))
+					setBlockedUsers(data.data.filter((user) => user.status === 2))
 				}
 			})
 		})
 	}
 
-	function getBlockedUsers() {
-		fetch(`https://192.168.2.235/api/blockedusers?jwt=${loginData.data.token}`).then((res) => {
-			res.json().then((data) => {
-				if (!data.hasOwnProperty('err') && data.hasOwnProperty('data')) {
-					setBlockedUsers(data.data)
-				}
-			})
-		})
-	}
-
-	function approveUser(e) {
+	function updateUserStatus(e) {
 		e.preventDefault()
-		let userID = e.target.id
-		console.log(userID)
-		fetch(`https://192.168.2.235/api/approveuser?id=${userID}&jwt=${loginData.data.token}`).then((res) => {
-			getUnverifiedUsers()
-		})
+		const userID = e.target.id
+		const status = e.target.value
+
+		if (window.confirm('Are you sure you want to update the user status?')) {
+			fetch(`https://192.168.2.235/api/updateUserStatus?status=${status}&id=${userID}&jwt=${loginData.data.token}`).then((res) => {})
+			getUsers()
+		}
 	}
+
 	return (
 		<>
 			<h3 style={{color: 'var(--dark-green)'}}>User Management</h3>
@@ -63,7 +46,7 @@ function Admin() {
 			<br></br>
 			<br></br>
 			<h1>Unverified Users</h1>
-			<div className={`${unverifiedUsers.length > 0 ? 'col-6 p-0 ml-5' : ''} `}>
+			<div className={`${unverifiedUsers.length > 0 ? 'col-10 p-0 ml-5 text-left' : ''} `}>
 				{unverifiedUsers.length > 0 ? (
 					<table className="table table-borderless ml-3" style={{minWidth: '130%'}}>
 						<thead>
@@ -91,15 +74,23 @@ function Admin() {
 
 						<tbody>
 							{unverifiedUsers.map((user) => (
-								<tr className="text-center my-2 py-2" key={user.id}>
-									<td className="pl-0">{user.email}</td>
+								<tr className="my-2 py-2" key={user.id}>
+									<td className="pl-0 ">{user.email}</td>
 									<td className="pl-0">{user.role}</td>
 									<td className="pl-0">{user.city}</td>
 									<td className="pl-0">{user.state}</td>
 									<td className="pl-0">{user.phone}</td>
-									<button className="btn btn-primary" onClick={approveUser} id={user.id}>
-										Approve
-									</button>
+									<div>
+										<button className="btn btn-primary mx-2" onClick={updateUserStatus} id={user.id} value="1">
+											Approve
+										</button>
+										<button className="btn btn-outline-danger mx-2" onClick={updateUserStatus} id={user.id} value="2">
+											Block
+										</button>
+										<button className="btn btn-outline-dark mx-2" onClick={updateUserStatus} id={user.id} value="3">
+											Delete
+										</button>
+									</div>
 								</tr>
 							))}
 						</tbody>
@@ -115,8 +106,8 @@ function Admin() {
 
 			<hr className="my-5" />
 
-			<h1>Blocked Users</h1>
-			<div className={`${blockedUsers.length > 0 ? 'col-6 p-0 ml-5' : ''} `}>
+			<h1 className="text-danger">Blocked Users</h1>
+			<div className={`${blockedUsers.length > 0 ? 'col-10 p-0 ml-5' : ''} `}>
 				{blockedUsers.length > 0 ? (
 					<table className="table table-borderless ml-3" style={{minWidth: '130%'}}>
 						<thead>
@@ -150,9 +141,11 @@ function Admin() {
 									<td className="pl-0">{user.city}</td>
 									<td className="pl-0">{user.state}</td>
 									<td className="pl-0">{user.phone}</td>
-									<button className="btn btn-primary" onClick={approveUser} id={user.id}>
-										Approve
-									</button>
+									<div>
+										<button className="btn btn-primary mx-2" onClick={updateUserStatus} id={user.id} value="1">
+											Approve
+										</button>
+									</div>
 								</tr>
 							))}
 						</tbody>
@@ -161,6 +154,64 @@ function Admin() {
 					<center>
 						<div className="bg-danger rounded p-2 d-flex justify-content-center align-items-center">
 							<h5>No Blocked Users Found</h5>
+						</div>
+					</center>
+				)}
+			</div>
+
+			<hr className="my-5" />
+
+			<h1 className="text-success">Verified Users</h1>
+			<div className={`${verifiedUsers.length > 0 ? 'col-10 p-0 ml-5' : ''} `}>
+				{verifiedUsers.length > 0 ? (
+					<table className="table table-borderless ml-3" style={{minWidth: '130%'}}>
+						<thead>
+							<tr className="text-center">
+								<th className="text-secondary" scope="col">
+									Email
+								</th>
+								<th className="text-secondary pl-0" scope="col">
+									Role
+								</th>
+								<th className="text-secondary" scope="col">
+									City
+								</th>
+								<th className="text-secondary" scope="col">
+									State
+								</th>
+								<th className="text-secondary" scope="col">
+									Phone
+								</th>
+								<th className="text-secondary" scope="col">
+									Approve
+								</th>
+							</tr>
+						</thead>
+
+						<tbody>
+							{verifiedUsers.map((user) => (
+								<tr className="text-center my-2 py-2" key={user.id}>
+									<td className="pl-0">{user.email}</td>
+									<td className="pl-0">{user.role}</td>
+									<td className="pl-0">{user.city}</td>
+									<td className="pl-0">{user.state}</td>
+									<td className="pl-0">{user.phone}</td>
+									<div>
+										<button className="btn btn-outline-secondary mx-2" onClick={updateUserStatus} id={user.id} value="0">
+											Unverify
+										</button>
+										<button className="btn btn-danger mx-2" onClick={updateUserStatus} id={user.id} value="2">
+											Block
+										</button>
+									</div>
+								</tr>
+							))}
+						</tbody>
+					</table>
+				) : (
+					<center>
+						<div className="bg-danger rounded p-2 d-flex justify-content-center align-items-center">
+							<h5>No Verified Users Found</h5>
 						</div>
 					</center>
 				)}
